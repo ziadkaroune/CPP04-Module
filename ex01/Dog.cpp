01b42910218fe5aa50c5af8a4e45f2df55d341b2
zivd <ziad99kr@gmail.com>
2025-01-06 17:36:57 +0100

    fix dog

--- ex01/Dog.cpp
+++ ex01/Dog.cpp
@@ -3,18 +3,25 @@
 Dog::Dog() : Animal()
 {
     this->_type = "Dog";
+    this->brain = new Brain();
     std::cout << "Dog class Constractor is Created "<< std::endl;
 }
 
-Dog::Dog(const Dog &copy) : Animal()
+Dog::Dog(const Dog &copy) : Animal(copy)
 {
+    this->brain = new Brain(*copy.brain) ;
     *this = copy;
 }
 Dog &Dog::operator=(const Dog &copy)
-{
-    if(this == &copy)
-        return *this;
-    this->_type = copy._type;
+
+{  this->_type = copy._type; 
+     if (this != &copy)
+    {
+        Animal::operator=(copy);
+        delete brain;
+        brain = new Brain(*copy.brain);
+    }
+    
     return *this;
 }
 void Dog::makeSound(void)const
@@ -23,5 +30,6 @@ void Dog::makeSound(void)const
 }
 Dog::~Dog()
 {
+    delete brain;
     std::cout << "Dog class Deonstractor is Created "<< std::endl;
 }
--- ex01/Dog.hpp
+++ ex01/Dog.hpp
@@ -1,10 +1,13 @@
 #ifndef Dog_HPP
 #  define Dog_HPP
 #include "Animal.hpp"
+#include "Brain.hpp"
+
 
 class Dog : public Animal
 {
     private:
+    Brain *brain;
     public:
         Dog();
         Dog(const Dog &copy);
